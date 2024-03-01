5206b1e2538792e8a6f8fc5acda33754fc1ab761
shwarz113 <shwarz13@yandex.ru>
2024-03-01 15:04:50 +0300

    build

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+            Hello, {tg.initDataUnsafe?.user.first_name}
           Edit <code>src/App.js</code> and save to reload.
         </p>
       </header>
