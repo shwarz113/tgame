b218cee2833ef7294b5d826dd1aaa2e74d447baa
shwarz113 <shwarz13@yandex.ru>
2024-03-01 11:42:41 +0300

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+const tg = window.Telegram.WebApp;
+
 function App() {
+  console.log('tg', tg);
   return (
     <div className="App">
       <header className="App-header">
