39e6fe0813e7fce101965c7c694e86487fb3199a
shwarz113 <shwarz13@yandex.ru>
2024-03-10 15:27:21 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,7 @@ function App() {
 
     return (
         <div className="App">
-            hello!
-            {/*<Header />*/}
+            <Header />
             {/*<MainContainer />*/}
         </div>
     );
