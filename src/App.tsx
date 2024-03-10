e28aac6d0923ab9549feb1e4ad1e922d3561899e
shwarz113 <shwarz13@yandex.ru>
2024-03-10 11:20:32 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,12 @@ function App() {
     console.log('tg', tg);
     useEffect(() => {
         tg?.expand();
+        location.reload();
     }, []);
 
     return (
         // <div className="App" style={{ maxHeight: tg?.viewportHeight || 'none'}}>
-        <div className="App" style={{ height: (tg?.viewportHeight - 30) || 'auto'}}>
+        <div className="App" style={{ maxHeight: (tg?.viewportHeight - 30) || 'auto'}}>
             <div className="App-header">
                 {/*<div>Hello, {tg.initDataUnsafe?.user?.first_name || 'cryptoBRO'}!</div>*/}
                 <ScoreHeader />
